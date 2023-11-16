9df408c16f84a28759717f2581879baef659314a
yusuf demiray <ydemiray015@gmail.com>
2023-11-17 00:33:04 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryGeneric
+{
+    internal class MyDictionary<K, V>
+    {
+        K[] keys, tempArray1;
+        V[] values, tempArray2;
+
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] tempArray1 = keys;
+            V[] tempArray2 = values;
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempArray1.Length; i++)
+            {
+                keys[i] = tempArray1[i];
+                values[i] = tempArray2[i];
+
+            }
+   
+             keys[keys.Length - 1] = key ;
+             values[values.Length - 1] = value;
+
+
+        }
+
+        public int Length
+        {
+            get { return keys.Length; }
+
+        }
+
+        public K[] _Keys 
+        {
+            get { return keys; }
+        }
+
+        public V[] _Values
+        {
+            get { return values; }
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace DictionaryGeneric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Dictionary<String,int> Cars = new Dictionary<String,int>();
+            //Cars.Add("BMW",1);
+
+            //Console.WriteLine(Cars[key:("BMW")]);
+            //Console.WriteLine("----------------------------------------");
+
+
+            MyDictionary<String,int> myDictionary = new MyDictionary<String,int>();
+            myDictionary.Add("Audi",2);
+            Console.WriteLine(myDictionary.Length);
+
+            for (int i = 0; i < myDictionary.Length; i++)
+            {
+                Console.WriteLine("Key : " + myDictionary._Keys[i] +" AND " + "Value : " + myDictionary._Values[i]  );
+
+            }
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
